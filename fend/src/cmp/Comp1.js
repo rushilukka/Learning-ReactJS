68fbf843cdade6f199079323dabeb61271cf9f3c
RushiLukka <rushi.lukka.14@gmail.com>
2024-12-25 02:18:31 +0530

    commit

---
+++ fend/src/cmp/Comp1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Comp1 = () => {
+  return (
+    <div style={{backgroundColor:'black',width:'300px',height:'200px'}}>
+        hello
+    </div>
+  )
+}
+
+export default Comp1
